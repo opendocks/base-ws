36fed2f6a534a13807472c6bb6f3b88f65bcf940
Syed Ali Murtaza <smurtazakazmi14@gmail.com>
2019-08-31 01:36:32 +0500

    Fixes travis script, to add tag to local image, to push on docker hub

--- travis-main.sh
+++ travis-main.sh
@@ -5,5 +5,5 @@ set -xe
 
 #### Build the Docker Images
 if [ -n "${PHP_VERSION}" ]; then
-    docker build -f ./Dockerfile-${PHP_VERSION} .
+    docker build -f ./Dockerfile-${PHP_VERSION} -t ${DOCKER_USERNAME}/base-php:${IMAGE_TAG} .
 fi
