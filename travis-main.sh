17debc14b997a0b3aae1c4637bf5a2a59e046686
Syed Ali Murtaza <ali@kazmi.me>
2022-04-23 14:12:30 +0500

    Adds support for php 8.1, removes travic ci files

--- travis-main.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#### halt script on error
-set -xe
-
-#### Build the Docker Images
-if [ -n "${FILE_IDENTIFIER}" ]; then
-    docker build -f ./Dockerfile-${FILE_IDENTIFIER} -t ${DOCKER_ORGANIZATION}/base-ws:${IMAGE_TAG} .
-fi
